bfe874b74ed162ecd7eb7d0120201fc1d198f883
ojflores <ojflores@puc.edu>
2018-02-19 09:39:01 +0000

    each can make a move

--- board.cpp
+++ board.cpp
@@ -21,17 +21,56 @@ void Board::draw_board(){
 			cout << "\n";
 		}
 	}
-	
+}
+
+bool Board::valid_move(int number){
+	if(the_board[number] != " X " && the_board[number] != " O "){
+		return true;
+	}
+	else if(number > 56){
+		cout << "returned false\n";
+		return false;
+	}
+	else{
+		return false;
+	}
 }
 
 void Board::start(){
+	int move = 0;
 	int turn = 2;
 	Player A("X");
 	Player B("O");
-	if(turn % 2 == 0){
-		A.get_move();
-	}
-	else{
-		B.get_move();
-	}
+	do{
+		if(turn % 2 == 0){
+			//subtracts 1 to because of array offset
+			move = A.get_move() - 1;
+			while(valid_move(move)){
+				move += 7;
+				if(move >= 56){
+					break;
+				}
+			}
+			
+			move -= 7;
+			cout << move << endl;
+			the_board[move] = " X ";
+			draw_board();
+		}
+		else{
+			//subtracts 1 to because of array offset
+			move = B.get_move() - 1;
+			while(valid_move(move)){
+				move += 7;
+				if(move >= 56){
+					break;
+				}
+			}
+			move -= 7;
+			cout << move << endl;
+			the_board[move] = " O ";
+			draw_board();
+		}
+		turn++;
+	}while(turn < 22);
 }
